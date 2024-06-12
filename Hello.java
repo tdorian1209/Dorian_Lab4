8090c1f9ac3eb82f31e74bb2b3ce5eda99cf6e5a
thomas <tdorian@umich.edu>
2024-06-12 15:49:21 -0400

    Comitting branch 1

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 public static void main(String[] args) {
 for(int i=1;i<=99;i++) {
 System.out.println("Hello World..."+i);
-System.out.println("Changed");
+System.out.println("This is branch 1");
 }
 }
 }
